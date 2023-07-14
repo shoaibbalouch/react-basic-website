5c6f4e2c814809673ae45859a1d5c60923a89085
shoaibbalouch <132067554+shoaibbalouch@users.noreply.github.com>
2023-07-14 06:24:06 -0700

    word counter website in react js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar(props) {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
+    <a className="navbar-brand" href="/">TextUtils</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -15,17 +15,29 @@ function Navbar() {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link" href="/">About</a>
         </li>
        
         <li className="nav-item">
-          <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div className='d-flex'>
+      <div className='bg-primary rounded mx-2'onClick={()=>{props.toggleMode('primary')} }style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+      <div className='bg-warning rounded mx-2'onClick={()=>{props.toggleMode('warning')} }style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+      <div className='bg-danger rounded mx-2'onClick={()=>{props.toggleMode('danger')} }style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+      <div className='bg-success rounded mx-2'onClick={()=>{props.toggleMode('success')} }style={{height:'30px',width:'30px',cursor:'pointer'}}></div>
+      
+
+      
+      
+      
+      
+      </div>
+      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+  <input className="form-check-input"onClick={()=>{props.toggleMode(null)}} type="checkbox" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">toggle mode</label>
+</div>
     </div>
   </div>
 </nav>  
